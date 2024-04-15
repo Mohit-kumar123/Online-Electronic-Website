21e6b9bd844a51c9e1630a96e21320551c98eea6
mohit <mk0733920@gmail.com>
2024-04-15 18:33:48 +0530

    online electronic website

---
+++ schema.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userDetailSchema = new mongoose.Schema({
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    phonenumber: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+})
+
+export default userDetailSchema;
